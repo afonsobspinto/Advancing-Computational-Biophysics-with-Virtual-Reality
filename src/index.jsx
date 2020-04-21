393261faacb2a95f6ad248e501f276b5311c1129
Afonso Pinto <afonsobspinto@gmail.com>
2020-04-21 22:03:30 +0100

    Add enter VR button

---
+++ src/index.jsx
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './components/App';
+
+const rootElement = document.getElementById('sceneContainer');
+ReactDOM.render(<App />, rootElement);
